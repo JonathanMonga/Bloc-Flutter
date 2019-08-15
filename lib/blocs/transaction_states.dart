c9fd7f77e354eca4ee0a104a46deac8e399781ca
Jonathan Monga <jmonga98@gmail.com>
2019-08-16 01:58:01 +0200

    add state classes

---
+++ lib/blocs/transaction_states.dart
@@ -0,0 +1,34 @@
+import 'package:bloc_app/data/models/transaction.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class TransactionState extends Equatable {
+  TransactionState([List props = const []]) : super(props);
+}
+
+class TransactionStateEmpty extends TransactionState {
+  @override
+  String toString() => 'TransactionStateEmpty';
+}
+
+class TransactionStateLoading extends TransactionState {
+  @override
+  String toString() => 'TransactionStateLoading';
+}
+
+class TransactionStateSuccess extends TransactionState {
+  final List<Transaction> items;
+
+  TransactionStateSuccess(this.items) : super([items]);
+
+  @override
+  String toString() => 'TransactionStateSuccess { items: ${items.length} }';
+}
+
+class TransactionStateError extends TransactionState {
+  final String error;
+
+  TransactionStateError(this.error) : super([error]);
+
+  @override
+  String toString() => 'TransactionStateError';
+}
