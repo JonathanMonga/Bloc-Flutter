522cc71cd83edafb3ab3b74db31fbaca7c0e7bad
Jonathan Monga <jmonga98@gmail.com>
2019-08-16 03:10:23 +0200

    change main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:bloc_app/blocs/transaction_bloc.dart';
-import 'package:bloc_app/blocs/transaction_states.dart';
 import 'package:bloc_app/data/client/transaction_client.dart';
 import 'package:bloc_app/data/repositories/repository.dart';
 import 'package:bloc_app/data/repositories/transaction_repository.dart';
 import 'package:bloc_app/data/storage/transaction_cache.dart';
 import 'package:bloc_app/ui/transaction_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:meta/meta.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   final Repository _transactionRepository = TransactionRepository(
