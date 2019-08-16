72c092af76eadd4254b1a84500ee784a2f4a0864
Jonathan Monga <jmonga98@gmail.com>
2019-08-16 03:07:51 +0200

    add main and transaction_widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,40 @@
+import 'package:bloc_app/blocs/transaction_bloc.dart';
+import 'package:bloc_app/blocs/transaction_states.dart';
+import 'package:bloc_app/data/client/transaction_client.dart';
+import 'package:bloc_app/data/repositories/repository.dart';
+import 'package:bloc_app/data/repositories/transaction_repository.dart';
+import 'package:bloc_app/data/storage/transaction_cache.dart';
+import 'package:bloc_app/ui/transaction_widget.dart';
+import 'package:flutter/material.dart';
 
+import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  final Repository _transactionRepository = TransactionRepository(
+      cache: TransactionCache(), client: TransactionClient());
+
+  runApp(App(transactionRepository: _transactionRepository));
+}
+
+class App extends StatelessWidget {
+  final TransactionRepository transactionRepository;
+
+  const App({
+    Key key,
+    @required this.transactionRepository,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Github Search',
+      home: Scaffold(
+          appBar: AppBar(title: Text('Github Search')),
+          body: BlocProvider<TransactionBloc>(
+            bloc: TransactionBloc(repository: transactionRepository),
+            child: TransactionWidget(),
+          )),
+    );
+  }
+}
---
+++ lib/ui/transaction_widget.dart
@@ -0,0 +1,92 @@
+import 'package:bloc_app/blocs/transaction_bloc.dart';
+import 'package:bloc_app/blocs/transaction_event.dart';
+import 'package:bloc_app/blocs/transaction_states.dart';
+import 'package:bloc_app/data/models/transaction.dart';
+import 'package:bloc_app/data/models/transaction_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TransactionWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[_TransactionListView()],
+    );
+  }
+}
+
+class _TransactionListView extends StatefulWidget {
+  @override
+  _TransactionListViewState createState() => _TransactionListViewState();
+}
+
+class _TransactionListViewState extends State<_TransactionListView> {
+  TransactionBloc _transactionBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _transactionBloc = BlocProvider.of<TransactionBloc>(context);
+
+    _transactionBloc.dispatch(LoadingEvent());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TransactionBloc, TransactionState>(
+        builder: (context, state) {
+      if (state is TransactionStateLoading) {
+        return CircularProgressIndicator();
+      }
+      if (state is TransactionStateError) {
+        return Text(state.error);
+      }
+      if (state is TransactionStateSuccess) {
+        return state.items.transactions.isEmpty
+            ? Text('No Results')
+            : Expanded(child: _TransactionResults(items: state.items));
+      }
+      return Text('Please enter a term to begin');
+    });
+  }
+}
+
+class _TransactionResults extends StatelessWidget {
+  final Transaction items;
+
+  const _TransactionResults({Key key, this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.transactions.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _TransactionResultItem(item: items.transactions[index]);
+      },
+    );
+  }
+}
+
+class _TransactionResultItem extends StatelessWidget {
+  final TransactionItem item;
+
+  const _TransactionResultItem({Key key, @required this.item})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(child: Text(item.prenom_dest.substring(0, 2))),
+      title: Text(item.type_jrn == "e"
+          ? "Envoie a ${item.prenom_dest} ${item.nom_dest}"
+          : "Reception de ${item.prenom_dest} ${item.nom_dest}"),
+      subtitle: Text("${item.montant_jrn} ${item.monnaie_jrn}"),
+      onTap: () async {},
+    );
+  }
+}
